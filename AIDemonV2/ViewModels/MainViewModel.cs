94aac15cc25111953cf8352483e1679741073d03
Jakub Kamiński <jakub.kaminski@intelimedical.pl>
2025-03-06 10:15:20 +0100

    Enhance MainViewModel and MainView UI components
    Added IsNotBusy and SendButtonIcon properties to MainViewModel for better state management. Implemented SendMessageAsync method for handling message sending logic. Updated MainView.axaml layout to improve user experience, including enhanced TextBox properties and a more informative send button with icon and text.

--- AIDemonV2/ViewModels/MainViewModel.cs
+++ AIDemonV2/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace AIDemonV2.ViewModels;
+﻿using ReactiveUI;
+using System.Threading.Tasks;
+
+namespace AIDemonV2.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
@@ -11,5 +14,32 @@ public class MainViewModel : ViewModelBase
 		LeftPanelViewModel = new LeftPanelViewModel();
 		ChatViewModel = new MainChatViewModel();
 		RightPanelViewModel = new RightPanelViewModel();
+	}
+
+	private bool _isNotBusy = true;
+	public bool IsNotBusy
+	{
+		get => _isNotBusy;
+		set => this.RaiseAndSetIfChanged(ref _isNotBusy, value);
+	}
+
+	private string _sendButtonIcon = "M0,0 L10,5 L0,10 Z"; // Ikona wysyłania (domyślna)
+	public string SendButtonIcon
+	{
+		get => _sendButtonIcon;
+		set => this.RaiseAndSetIfChanged(ref _sendButtonIcon, value);
+	}
+
+	// metoda przykładowa zmieniająca ikonę i stan
+	private async Task SendMessageAsync()
+	{
+		IsNotBusy = false;
+		SendButtonIcon = "M5,0 L5,10 M0,5 L10,5"; // np. ikona oczekiwania
+
+		// tutaj logika wysyłania wiadomości
+		await Task.Delay(1000);
+
+		SendButtonIcon = "M0,0 L10,5 L0,10 Z"; // przywrócenie ikony wysyłania
+		IsNotBusy = true;
 	}
 }
